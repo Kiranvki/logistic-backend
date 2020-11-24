20e0b368f59f575bb049b1439d3661126a54ab38
Krishna Agrawal <krishna.agrawal@waycool.in>
2020-11-24 11:28:20 +0530

    removed .env file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [{
-    name: 'API DMS-V2 USER PORT:3000',
+    name: 'API DMS-V2 LOGISTICS PORTAL : 3003',
     script: './server.js',
     watch: false,
     env: {
       'watch': false,
-      'PORT': 3000,
+      'PORT': 3003,
       'NODE_ENV': 'development',
-      'DB': 'mongodb://localhost:27017/dms-user',
+      'DB': 'mongodb://localhost:27017/dms-picker',
       'secret': '975f056b4552226360a6f808c1a1ff57e4ee07d9347479e8afd1d9ed8147eebf6b9717c262727b286555e2a3b427d33bb88766d150956c7778e06f28a08f5e1c573e6d15831c6108c6567822ae353355ce7b00a8c5547d6d7340a8cf9643f20a3d3affa8f3100d1e9c600e1257a1f7ccd41d9e064efaca490c955e708fb901e3bff58839b251b6d684acd0d788419cc74cface45ba58cca6a360ed87ff447bd0c49204cfaad3f64e3b4845decd05cd51c5ef093498e8425bb978d6530e13067d43114b4d0561a9cd73c654f5123a35df747b19a64a5bc30efa59091cb94fbcd58d4dac4cec261763f2a3c59e71c72d9e74366b2ad0e52a97c56083aa0a41cbfe',
       'frontEndHost': 'http://uat.apps.waycool.in:',
       'frontEndPort': '3032/',
@@ -17,13 +17,19 @@ module.exports = {
       'adminBaseUrl': 'http://localhost:3001',
       'adminGetBrandDetails': '/internal/v1/brands/manager/',
       'brandFrontEndHost': 'http://uat.apps.waycool.in:',
-      'brandFrontEndPort': '3033/'
+      'brandFrontEndPort': '3033/',
+      'otpUrl': 'https://api.msg91.com/api/v5/otp',
+      'otpBalUrl': 'https://api.msg91.com/api/balance.php',
+      'authkey': '178070A6VsiNjrpi659d6e874',
+      'otpType': '106',
+      'appSecret': '975f056b4552229451a6f808c1a1ff57e4ee07d9347479ar1d3ad9ed36aseebf6b9717c262727b286555e2a3b427d33bb88766d150956c7778e06f28a08f5e1c573e6d15831c6108c6567822ae353355ce7b00a8c5547d6d7340a8cf9643f20a3d3affa8f3100d1e9c600e1257a1f7ccd41d9e064efaca490c955e708fb901e3bff58839b251b6d684acd0d788419cc74cface45ba58cca6a360ed87ff447bd0c49204cfaad3f64e3b4845decd05cd51c5ef093498e8425bb978d6530e13067d43114b4d0561a9cd73c654f5123a35df747b19a64a5bc30efa59091cb94fbcd58d4dapy6w261763f2a3c59e71c72d9e7asp5b2ad0e52a97c31243aa0a41cbfe',
+      'sendSms': 0,
     },
     env_staging: {
       'watch': false,
-      'PORT': 3000,
+      'PORT': 3003,
       'NODE_ENV': 'development',
-      'DB': 'mongodb://localhost:27017/dms-user',
+      'DB': 'mongodb://localhost:27017/dms-picker',
       'secret': '975f056b4552226360a6f808c1a1ff57e4ee07d9347479e8afd1d9ed8147eebf6b9717c262727b286555e2a3b427d33bb88766d150956c7778e06f28a08f5e1c573e6d15831c6108c6567822ae353355ce7b00a8c5547d6d7340a8cf9643f20a3d3affa8f3100d1e9c600e1257a1f7ccd41d9e064efaca490c955e708fb901e3bff58839b251b6d684acd0d788419cc74cface45ba58cca6a360ed87ff447bd0c49204cfaad3f64e3b4845decd05cd51c5ef093498e8425bb978d6530e13067d43114b4d0561a9cd73c654f5123a35df747b19a64a5bc30efa59091cb94fbcd58d4dac4cec261763f2a3c59e71c72d9e74366b2ad0e52a97c56083aa0a41cbfe',
       'frontEndHost': 'http://uat.apps.waycool.in:',
       'frontEndPort': '3032/',
@@ -33,13 +39,19 @@ module.exports = {
       'adminBaseUrl': 'http://localhost:3001',
       'adminGetBrandDetails': '/internal/v1/brands/manager/',
       'brandFrontEndHost': 'http://uat.apps.waycool.in:',
-      'brandFrontEndPort': '3033/'
+      'brandFrontEndPort': '3033/',
+      'otpUrl': 'https://api.msg91.com/api/v5/otp',
+      'otpBalUrl': 'https://api.msg91.com/api/balance.php',
+      'authkey': '178070A6VsiNjrpi659d6e874',
+      'otpType': '106',
+      'appSecret': '975f056b4552229451a6f808c1a1ff57e4ee07d9347479ar1d3ad9ed36aseebf6b9717c262727b286555e2a3b427d33bb88766d150956c7778e06f28a08f5e1c573e6d15831c6108c6567822ae353355ce7b00a8c5547d6d7340a8cf9643f20a3d3affa8f3100d1e9c600e1257a1f7ccd41d9e064efaca490c955e708fb901e3bff58839b251b6d684acd0d788419cc74cface45ba58cca6a360ed87ff447bd0c49204cfaad3f64e3b4845decd05cd51c5ef093498e8425bb978d6530e13067d43114b4d0561a9cd73c654f5123a35df747b19a64a5bc30efa59091cb94fbcd58d4dapy6w261763f2a3c59e71c72d9e7asp5b2ad0e52a97c31243aa0a41cbfe',
+      'sendSms': 0,
     },
     env_production: {
       'watch': false,
-      'PORT': 3000,
+      'PORT': 3003,
       'NODE_ENV': 'development',
-      'DB': 'mongodb://localhost:27017/dms-user',
+      'DB': 'mongodb://localhost:27017/dms-picker',
       'secret': '975f056b4552226360a6f808c1a1ff57e4ee07d9347479e8afd1d9ed8147eebf6b9717c262727b286555e2a3b427d33bb88766d150956c7778e06f28a08f5e1c573e6d15831c6108c6567822ae353355ce7b00a8c5547d6d7340a8cf9643f20a3d3affa8f3100d1e9c600e1257a1f7ccd41d9e064efaca490c955e708fb901e3bff58839b251b6d684acd0d788419cc74cface45ba58cca6a360ed87ff447bd0c49204cfaad3f64e3b4845decd05cd51c5ef093498e8425bb978d6530e13067d43114b4d0561a9cd73c654f5123a35df747b19a64a5bc30efa59091cb94fbcd58d4dac4cec261763f2a3c59e71c72d9e74366b2ad0e52a97c56083aa0a41cbfe',
       'frontEndHost': 'http://uat.apps.waycool.in:',
       'frontEndPort': '3032/',
@@ -49,7 +61,13 @@ module.exports = {
       'adminBaseUrl': 'http://localhost:3001',
       'adminGetBrandDetails': '/internal/v1/brands/manager/',
       'brandFrontEndHost': 'http://uat.apps.waycool.in:',
-      'brandFrontEndPort': '3033/'
+      'brandFrontEndPort': '3033/',
+      'otpUrl': 'https://api.msg91.com/api/v5/otp',
+      'otpBalUrl': 'https://api.msg91.com/api/balance.php',
+      'authkey': '178070A6VsiNjrpi659d6e874',
+      'otpType': '106',
+      'appSecret': '975f056b4552229451a6f808c1a1ff57e4ee07d9347479ar1d3ad9ed36aseebf6b9717c262727b286555e2a3b427d33bb88766d150956c7778e06f28a08f5e1c573e6d15831c6108c6567822ae353355ce7b00a8c5547d6d7340a8cf9643f20a3d3affa8f3100d1e9c600e1257a1f7ccd41d9e064efaca490c955e708fb901e3bff58839b251b6d684acd0d788419cc74cface45ba58cca6a360ed87ff447bd0c49204cfaad3f64e3b4845decd05cd51c5ef093498e8425bb978d6530e13067d43114b4d0561a9cd73c654f5123a35df747b19a64a5bc30efa59091cb94fbcd58d4dapy6w261763f2a3c59e71c72d9e7asp5b2ad0e52a97c31243aa0a41cbfe',
+      'sendSms': 1,
     }
   }]
 };
