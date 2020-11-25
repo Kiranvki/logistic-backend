4e6fb177acab05728fed21695288a9948f62c5d3
Krishna Agrawal <krishna.agrawal@waycool.in>
2020-11-25 11:41:27 +0530

    added sales order model

---
+++ src/components/sales_order/sales_order/models/sales_order.model.js
@@ -0,0 +1,271 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// schema
+const salesOrder = new Schema({
+
+  'orderPK': {
+    type: 'Number'
+  },
+  'onlineReferenceNo': {
+    type: 'Number'
+  },
+  'onlineChildReferenceNo': {
+    type: 'String'
+  },
+  'createdAt': {
+    type: 'Date'
+  },
+  'updatedAt': {
+    type: 'Date'
+  },
+  'status': {
+    type: 'String'
+  },
+  'orderRemarks': {
+    type: 'String'
+  },
+  'Channel': {
+    type: 'String'
+  },
+  'totalQuantity': {
+    type: 'Number'
+  },
+  'totalFreeQty': {
+    type: 'Number'
+  },
+  'totalAmount': {
+    type: 'Number'
+  },
+  'paymentMode': {
+    type: 'String'
+  },
+  'totalTaxAmount': {
+    type: 'Number'
+  },
+  'totalDiscountAmount': {
+    type: 'Number'
+  },
+  'courierPartner': {
+    type: 'String'
+  },
+  'shippingId': {
+    type: 'String'
+  },
+  'shippingName': {
+    type: 'String'
+  },
+  'shippingAddress1': {
+    type: 'String'
+  },
+  'shippingAddress2': {
+    type: 'String'
+  },
+  'shippingPlace': {
+    type: 'String'
+  },
+  'shippingState': {
+    type: 'String'
+  },
+  'shippingCountry': {
+    type: 'String'
+  },
+  'shippingPincode': {
+    type: 'String'
+  },
+  'shippingPhone': {
+    type: 'String'
+  },
+  'shippingMobile': {
+    type: 'String'
+  },
+  'shippingEmail': {
+    type: 'String'
+  },
+  'shippingCharge': {
+    type: 'Number'
+  },
+  'packingCharge': {
+    type: 'Number'
+  },
+  'shippingMethod': {
+    type: 'String'
+  },
+  'ShipmentPointsUsed': {
+    type: 'Number'
+  },
+  'shipmentItems': {
+    type: 'Number'
+  },
+  'shipmentABN': {
+    type: 'String'
+  },
+  'shipmentWeight': {
+    type: 'Number'
+  },
+  'latitude': {
+    type: 'String'
+  },
+  'longitude': {
+    type: 'String'
+  },
+  'customeLatitude': {
+    type: 'String'
+  },
+  'customerLongitude': {
+    type: 'String'
+  },
+  'discountCoupon': {
+    type: 'String'
+  },
+  'deliveryDate': {
+    type: 'Date'
+  },
+  'locationId': {
+    type: 'Number'
+  },
+  'userId': {
+    type: 'String'
+  },
+  'appUserName': {
+    type: 'String'
+  },
+  'customerCode': {
+    type: 'Number'
+  },
+  'customerId': {
+    type: 'String'
+  },
+  'customerName': {
+    type: 'String'
+  },
+  'customerType': {
+    type: 'String'
+  },
+  'customerAddressLine1': {
+    type: 'String'
+  },
+  'customerAddressLine2': {
+    type: 'String'
+  },
+  'customerAddressLine3': {
+    type: 'String'
+  },
+  'customerArea': {
+    type: 'String'
+  },
+  'customerCity': {
+    type: 'String'
+  },
+  'customerState': {
+    type: 'String'
+  },
+  'customerCountry': {
+    type: 'String'
+  },
+  'customerPincode': {
+    type: 'String'
+  },
+  'customerPhone': {
+    type: 'String'
+  },
+  'customerMobile': {
+    type: 'String'
+  },
+  'customerEmail': {
+    type: 'String'
+  },
+  'ordTimestamp': {
+    type: 'Number'
+  },
+  'outletId': {
+    type: 'Number'
+  },
+  'isOfflineOrder': {
+    type: 'Number'
+  },
+  'invoiceNo': {
+    type: 'String'
+  },
+  'deliveryBoy': {
+    type: 'String'
+  },
+  'deilveryBoyMobileNo': {
+    type: 'String'
+  },
+  'otherChargesTaxAmount': {
+    type: 'Number'
+  },
+  'otherChargesTaxPercentage': {
+    type: 'Number'
+  },
+  'otherChargesTaxInclusive': {
+    type: 'String'
+  },
+  'orderItems': [
+    {
+      'rowNo': {
+        type: 'Number'
+      },
+      'id': {
+        type: 'Number'
+      },
+      'itemId': {
+        type: 'Number'
+      },
+      'itemName': {
+        type: 'String'
+      },
+      'itemReferenceCode': {
+        type: 'String'
+      },
+      'salePrice': {
+        type: 'Number'
+      },
+      'quantity': {
+        type: 'Number'
+      },
+      'suppliedQty': {
+        type: 'String'
+      },
+      'itemAmount': {
+        type: 'Number'
+      },
+      'iBarU': {
+        type: 'Number'
+      },
+      'taxPercentage': {
+        type: 'Number'
+      },
+      'itemTaxType': {
+        type: 'String'
+      },
+      'discountPercentage': {
+        type: 'Number'
+      },
+      'itemRemarks': {
+        type: 'String'
+      },
+      'itemMarketPrice': {
+        type: 'Number'
+      },
+      'freeQty': {
+        type: 'Number'
+      },
+      'orderPK': {
+        type: 'Number'
+      },
+    }
+  ],
+
+}, {
+  timestamps: true
+});
+
+salesOrder.index({
+  'onlineReferenceNo': 1,
+  'createdAt': 1
+});
+
+// exporting the entire module
+module.exports = mongoose.model('salesOrder', salesOrder);
---
+++ src/components/sales_order/sales_order/sales_order.router.js
@@ -0,0 +1,123 @@
+// user controller 
+const ctrl = require('./sales_order.controller');
+
+// library
+const multer = require('multer');
+const multipartMiddleware = multer();
+
+// custom joi validation
+const {
+  joiTallySync, // joi tally sync
+  joiTallyUpload, // joi tally upload 
+  joiCustomerGet, // joi customer get 
+  joiGoFrugalSync, // sync data with gofrugal 
+  joiCustomersList, // get the list of customers in db
+  joiGetCustomerOther, // get customer other details 
+  joiInvoicePaymentRefresh, // joi invoice payment sync 
+} = require('./sales_order.validators');
+
+// hooks 
+const {
+  isValidCustomer, // check whether the customer is valid or not 
+  setupDataForGoFrugalApi, // setup the gofrugal api
+  setupDataForTallyOtherApi, // setup data for tally api
+  getTheDetailsFromGoFrugal, // get the details from go frugal
+  readCsvForCustomerDataSync, // read tally csv data for customer data sync 
+  getCustomerIdsBasedOnFiltering, // get the customer ids based on filtering 
+  getTheCustomerDetailsAsPerType, // get the customer details as per the type 
+  getTheOtherDetailsFromTallyServer, // get the details from the tally server 
+  checkWhetherCustomerListIsAlreadySyncing, // check whether customer list is already syncing 
+  getAllTheInvoicesAndRefreshAsPerThePaymentReceived, // get all invoices and refresh as per the payment received
+} = require('../../../hooks');
+
+// auth 
+const {
+  verifyUserToken
+} = require('../../../hooks/Auth');
+
+// exporting the user routes 
+function userRoutes() {
+  //open, closed
+  return (open, closed) => {
+
+    // sync with goFrugal  
+    open.route('/goFrugal/sync/city/:city').get(
+      [joiGoFrugalSync], // joi validation
+      setupDataForGoFrugalApi, // setup data for gofrugal
+      getTheDetailsFromGoFrugal, // get the data from go frugal 
+      ctrl.syncWithGoFrugal // get controller 
+    );
+
+    // sync with goFrugal  
+    closed.route('/goFrugal/sync').get(
+      verifyUserToken, // verify user access token
+      checkWhetherCustomerListIsAlreadySyncing, // check whether the data is already syncing or not 
+      setupDataForGoFrugalApi, // setup data for gofrugal
+      getTheDetailsFromGoFrugal, // get the data from go frugal 
+      ctrl.syncWithGoFrugal // get controller 
+    );
+
+    // post 
+    closed.route('/:customerId').get(
+      [joiCustomerGet], // joi validation
+      verifyUserToken, // verify user token
+      isValidCustomer, // check whether the customer is valid or not
+      ctrl.getCustomerDetails // get controller 
+    );
+
+    // get all 
+    closed.route('/').post(
+      [joiCustomersList], // joi validation
+      verifyUserToken, // verify user token
+      getCustomerIdsBasedOnFiltering, // get the customer ids based on filtering 
+      ctrl.getList // get controller 
+    );
+
+    // get minified list
+    closed.route('/minified/list').get(
+      [joiCustomersList], // joi validation
+      verifyUserToken, // verify user token
+      ctrl.getMinifiedList // get controller 
+    );
+
+    // get minified list
+    open.route('/minified/list/city/:city').get(
+      [joiCustomersList], // joi validation
+      // verifyUserToken, // verify user token
+      ctrl.getMinifiedList // get controller 
+    );
+
+    // get the payment for each and every customer 
+    open.route('/tally/sync/:type/city/:city').get(
+      [joiTallySync], // joi validation
+      setupDataForTallyOtherApi, // setup data for gofrugal
+      getTheOtherDetailsFromTallyServer, // get the data from go frugal 
+      ctrl.syncWithTallyOthers // get controller 
+    );
+
+    // tally csv upload 
+    closed.route('/tally/sync/city/:city/type/:type/upload-csv').post(
+      multipartMiddleware.single('file'), // multer middleware
+      [joiTallyUpload], // joi validation
+      readCsvForCustomerDataSync, // fetch the data from the csv file
+      ctrl.syncWithTallyOthers // get controller 
+    );
+
+    // invoice payment sync 
+    open.route('/tally/invoice-payment/refresh/city/:city').get(
+      [joiInvoicePaymentRefresh], // joi validation
+      getAllTheInvoicesAndRefreshAsPerThePaymentReceived, // fetch the data from the csv file
+    );
+
+    // customer invoices / debit / credit / payment 
+    closed.route('/:customerId/:type').get(
+      [joiGetCustomerOther], // joi get customer other details joi validation
+      verifyUserToken, // verify user token
+      isValidCustomer, // check whether the customer is valid or not
+      getTheCustomerDetailsAsPerType, // get the customer details as per type
+      ctrl.getCustomerOtherDetails // get customer other details 
+    )
+  };
+}
+
+module.exports = userRoutes();
