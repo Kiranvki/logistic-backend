13fe2087cefc350893da8dd1628f11873bcf06ab
Krishna Agrawal <krishna.agrawal@waycool.in>
2020-12-15 14:28:05 +0530

    removed unnecessary hook file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,13 +5,5 @@ module.exports = {
   setupDataForGoFrugalApi: require('./setupDataForGoFrugalApi'),// setup the api for gofrugal  
   isValidPickerBoyId: require('./app/isValidPickerBoyId'),  //check whether the picker id is correct or not
   generateOtp: require('./app/generateOtp'), // generate the OTP
-  hashPassword: require('./hashPassword'), // hash password 
-  sendForgetEmail: require('./sendForgetEmail'), // send forget password email
-  generateRandomHashPasswordArray: require('./generateRandomHashPasswordArray'), // generate random hash password
-  sendInviteEmail: require('./sendInviteEmail'), // send invite email
-  encryptTheFileInorderToStoreItInDb: require('./encryptTheFileInorderToStoreItInDb'), // encrypt the file inorder to store the file in the db
-  getDecryptedImageBuffer: require('./getDecryptedImageBuffer'), // get the decrypted images
-  generateRandomHashPasswordArrayForClosed: require('./generateRandomHashPasswordArrayForClosed'), // generate random hash for user onboard 
-  sendInviteEmailForClosed: require('./sendInviteEmailForClosed'), // send invite email for closed users
   isValidAgencyId: require('./isValidAgencyId') //check whether the agencyId is valid or not
 }
