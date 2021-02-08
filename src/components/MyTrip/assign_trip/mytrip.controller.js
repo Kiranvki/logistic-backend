172570d2a461ed74afa6bbb875dc90864d2b245b
raunik <raunik.singh@waycool.in>
2021-02-08 14:55:35 +0530

    get salesman and item api

--- src/components/MyTrip/assign_trip/mytrip.controller.js
+++ src/components/MyTrip/assign_trip/mytrip.controller.js
@@ -12,6 +12,7 @@ const tripModel = require('../assign_trip/model/trip.model');
 const transporterModel = require('../../transporter/transporter/models/transporter.model');
 const transVehicleModel = require('../../rate_category/ratecategory_transporter_vehicle_mapping/models/ratecategory_transporter_vehicle_mapping.model')
 const _ = require('lodash');
+const request = require('request-promise');
 
 class MyTrip extends BaseController {
 
@@ -139,13 +140,15 @@ class MyTrip extends BaseController {
                 s: 0,
                 millisecond: 0
               }).toDate();
-
-              let getTransporter = await transporterModel.find({ 'vehicleDetails.name': { $regex: req.query.transporterName, $options: 'i' } }).select('_Id'); 
-              getTransporter = await getTransporter.map((v)=> {return v._id})
-              // console.log(getTransporter);
-              // transVehicleModel
+              if (!req.body.vehicleId) req.body.vehicleId = [];
+              let getTransporter = await transporterModel.find({ 'vehicleDetails.name': { $regex: req.body.transporterName || '', $options: 'i' } }).select('_Id'); 
+              getTransporter = await getTransporter.map((v)=> { return v._id })
+              
+              let vehicleIds = await transVehicleModel.find({transporterId: { $in: getTransporter } } ).select('vehicleId');
+              vehicleIds = await vehicleIds.map( ( v ) => { return v.vehicleId });
 
         let alreadyCheckInVehicleIds = await vehicleCheckedInModel.find({
+            'vehicleId': { $in: vehicleIds },
             'status': 1,
             'isDeleted': 0,
             'inTrip': 0,
@@ -155,8 +158,8 @@ class MyTrip extends BaseController {
             },
           }).lean();
 
+          
           alreadyCheckInVehicleIds = await alreadyCheckInVehicleIds.map((v) => v.vehicleId);
-         alreadyCheckInVehicleIds = req.body.alreadyCheckInVehicleIds || [];
         
         let page = req.query.page || 1,
         pageSize = await BasicCtrl.GET_PAGINATION_LIMIT().then((res) => { if (res.success) return res.data; else return 60; }),
@@ -348,6 +351,10 @@ class MyTrip extends BaseController {
       },
       ]).allowDiskUse(true);
 
+      for (let v of req.body.vehicleId) {
+        _.remove(vehicleList, (o) => { return o._id.toString() === v.toString(); });
+      };
+      
       return this.success(req, res, this.status.HTTP_OK, {
         results: vehicleList,
         pageMeta: {
@@ -358,24 +365,29 @@ class MyTrip extends BaseController {
       });
 
         } catch (error) {
-            error(error);
+            console.log(error)
             this.errors(req, res, this.status.HTTP_INTERNAL_SERVER_ERROR, this.exceptions.internalServerErr(req, err));
         }
     };
 
     createTrip = async (req, res) => {
         try {
-                req.body.tripId = await tripModel.countDocuments() + 1; // Mandatory to create sequence incremental unique Id
-                if (!req.body.tripId) return false;
-            
-                let trip = await tripModel.create(req.body);
-                
+                let trip = {}, transporterDetails = req.body.transporterDetails, orderArray = [], vehicleArray = [], trip_ids=[];
+
+                for ( let v of transporterDetails ) {
+
+                  req.body.tripId = await tripModel.countDocuments() + 1; // Mandatory to create sequence incremental unique Id
+                  if (!req.body.tripId) return false;
+                  
+                  req.body.transporterDetails = v
+              
+                  trip = await tripModel.create(req.body);
+                  trip_ids.push(trip._id);
+              
                 let orders = await tripModel.findOne({ _id: trip._id })
                                    .populate('vehicleId rateCategoryId checkedInId salesOrderId deliveryExecutiveId invoice_db_id')
                                    .populate({ path: 'vehicleId', populate: {path: 'rateCategoryId'} })
-                                   .lean();                
-                  
-                let orderArray = [], vehicleArray = [];
+                                   .lean();
 
                 for (let so of orders.salesOrderId) {
                     
@@ -388,8 +400,6 @@ class MyTrip extends BaseController {
 
                     let startOfTheDay = (DeliveryDate + " 00 00 00").toString();
                     let endOfTheDay = (DeliveryDate + " 23 59 00").toString();
-                    
-                    console.log("startOfTheDay:", startOfTheDay, 'endOfTheDay:', endOfTheDay)
                     
                     orderObj = {
                         customerName: so.customerName,
@@ -447,19 +457,20 @@ class MyTrip extends BaseController {
                     vehicleArray.push(vehicleObj);
                 };
 
+              };
+
                 for (let v of req.body.checkedInId ) {
                   await vehicleCheckedInModel.findOneAndUpdate({ _id: v }, { $set: { inTrip: 1 } } ); 
                 };
 
-                for (let v of req.body.transporterDetails) {
+                for (let v of transporterDetails) {
                   await deliveryExecModel.findOneAndUpdate({ _id: v.deliveryExecutiveId }, { $set: { inTrip: 1 } }); 
-                }
+                };
                 
-                return this.success(req, res, this.status.HTTP_OK, {orderArray, vehicleArray, orders}, 'Trip Created !');    
+                return this.success(req, res, this.status.HTTP_OK, {trip_ids, orderArray, vehicleArray}, 'Trip Created !');    
             
             } catch (error) {
             console.log(error)
-            error(error);
             this.errors(req, res, this.status.HTTP_INTERNAL_SERVER_ERROR, this.exceptions.internalServerErr(req, error));
         }
     };
@@ -496,10 +507,168 @@ class MyTrip extends BaseController {
           });
 
       } catch (error) {
-        error(error);
         this.errors(req, res, this.status.HTTP_INTERNAL_SERVER_ERROR, this.exceptions.internalServerErr(req, error));
       };
     };
+
+    getTrip = async (req, res) => {
+      try {
+        
+        let trips = await tripModel.find({_id: { $in: req.body.tripIds } } )
+                          .populate('vehicleId rateCategoryId checkedInId salesOrderId deliveryExecutiveId invoice_db_id')
+                          .populate({ path: 'vehicleId', populate: { path: 'rateCategoryId' } })
+                          .lean();
+
+        return this.success(req, res, this.status.HTTP_OK, { result: trips });
+
+      } catch (error) {
+        this.errors(req, res, this.status.HTTP_INTERNAL_SERVER_ERROR, this.exceptions.internalServerErr(req, error));
+      };
+    };
+
+    spotSales = async (req, res) => {
+      try {
+         
+      } catch (error) {
+        this.errors(req, res, this.status.HTTP_INTERNAL_SERVER_ERROR, this.exceptions.internalServerErr(req, error));
+      };
+    };
+
+    getSalesMan = async (req, res) => {
+
+      try {
+        let response = {};
+
+        let options = {
+          method: 'POST',
+          uri: '',
+          headers: {
+            //'x-auth-token': url['x-auth-token'], 
+        },
+          body: { salesManIds: [] },
+          json: true
+        };
+
+        // response = await request(options);
+
+        response = {
+          "status": 200,
+          "message": "Salesman List Fetched Successfully !",
+          "data": {
+              "results": [{
+                  "_id": "601e4756021a634cf75bed03",
+                  "location": {
+                      "coordinates": []
+                  },
+                  "employerName": "Waycool Foods & Products Private Limited",
+                  "isWaycoolEmp": 1,
+                  "profilePic": null,
+                  "recServerEmpId": null,
+                  "status": 1,
+                  "isDeleted": 0,
+                  "employeeId": "WFP/1637/21",
+                  "email": "anand.g@waycool.in",
+                  "gender": "",
+                  "aadharNumber": "0",
+                  "zohoId": "378158000017386050",
+                  "designation": "Senior Associate",
+                  "pan": "",
+                  "firstName": "Anand",
+                  "lastName": "G",
+                  "contactMobile": 9704410810,
+                  "photo": "https://contacts.zoho.com/file?ID=738723471&fs=thumb",
+                  "role": "Team member",
+                  "employeeStatus": "Active",
+                  "employeeType": "Permanent",
+                  "locationName": "Bangalore",
+                  "dateOfJoining": "2021-01-20T00:00:00.000Z",
+                  "reportingTo": {
+                      "id": "378158000010955069",
+                      "name": "Raja Achutha Naidu Kottana WFP/905/20",
+                      "emailId": "rajanaidu@waycool.in"
+                  },
+                  "agencyId": null,
+                  "fullName": "Anand G",
+                  "createdById": "5f89c48fcfafb9ff432dacd6",
+                  "createdBy": "rajanaidu@waycool.in",
+                  "warehouseId": "5f7aa79c34417a65b7eb66a1",
+                  "cityId": "bangalore",
+                  "createdAt": "2021-02-06T07:37:58.206Z",
+                  "updatedAt": "2021-02-06T07:37:58.206Z",
+                  "__v": 0,
+                  "asmMapping": {
+                      "_id": "601e4756021a634cf75bed04",
+                      "status": 1,
+                      "isDeleted": 0,
+                      "asmId": {
+                          "_id": "5fbf71a8d05b3c4626b7467e",
+                          "status": 1,
+                          "isDeleted": 0,
+                          "employeeId": "WFP/1091/20",
+                          "email": "raghavendra.n@waycool.in",
+                          "gender": "male",
+                          "designation": "Senior Associate Sales",
+                          "firstName": "Raghavendra",
+                          "lastName": "N",
+                          "contactMobile": 6363222900,
+                          "photo": "https://contacts.zoho.com/file?ID=724322711&fs=thumb"
+                      },
+                      "salesmanId": "601e4756021a634cf75bed03"
+                  },
+                  "numOfCustomersForTheDay": 18
+              }],
+              "pageMeta": {
+                  "skip": 0,
+                  "pageSize": 10,
+                  "total": 56
+              }
+          }
+        };
+
+          let salesMan = response.data.results;
+
+          return this.success(req, res, this.status.HTTP_OK, { result: salesMan });
+
+      } catch (error) {
+        console.log(error);
+        this.errors(req, res, this.status.HTTP_INTERNAL_SERVER_ERROR, this.exceptions.internalServerErr(req, error));
+      };
+    };
+
+    getItemsByLocation = async (req, res) => {
+      try {
+
+        let cityId =  req.query.cityId || 'chennai', limit = 10, page = 1, skipRec = 0;
+
+          limit = !req.query.limit ? limit = limit : limit = parseInt(req.query.limit);
+          page = !req.query.page ? page = page - 1 : page = parseInt(req.query.page) - 1; 
+        
+          skipRec = page * limit;
+
+          let query = { cityId: cityId };
+
+          if (req.query.searchText) query = { cityId: cityId, itemName: { '$regex': req.query.searchText, '$options': 'i' } }; 
+
+        
+        const MongoClient = require('mongodb').MongoClient;
+        let url = "mongodb://admin:myadminpassword@40.65.152.232:27017/";
+  
+        let db = await MongoClient.connect(url, { useUnifiedTopology: true });
+        let dbo = db.db("dms");
+        
+        let items = await dbo.collection("itemmasters").find(query).skip(skipRec).limit(limit).toArray();
+
+        await db.close();
+
+        return this.success(req, res, this.status.HTTP_OK, { result: items });
+
+      } catch (error) {
+
+      }
+    };
+
+    
+
 
 };
 
