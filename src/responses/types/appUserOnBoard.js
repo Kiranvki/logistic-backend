05fb5aa886f4442d3d37a45982ace17573930e49
Krishna Agrawal <krishna.agrawal@waycool.in>
2021-02-10 11:14:34 +0530

    change the message for sending the otp

--- src/responses/types/appUserOnBoard.js
+++ src/responses/types/appUserOnBoard.js
@@ -6,12 +6,12 @@ module.exports = {
     return `Error : ${error} !`;
   },
   unableToSendEmailOtp: 'Unable to send Email OTP at this moment !',
-  otpRegeneratedSuccessfully: 'Otp Send Successfully!',
+  otpRegeneratedSuccessfully: 'Otp Sent Successfully!',
   unableToGenerateOtpRightNow: 'Unable to generate OTP!',
   userLoginRequestStillNotRegenerated: 'User Still not requested for Login OTP!',
   invalidOtp: 'OTP entered is not valid or expired !',
   userLoggedInSuccessfully: 'User Logged In Successfully !',
   unableToVerifyOtp: 'Unable to verfiy OTP right now, Please try again after sometime !',
-  userCreatedSuccessfully : 'User Created Successfully !',
-  userNotCreated : "User Not Created Successfully !"
+  userCreatedSuccessfully: 'User Created Successfully !',
+  userNotCreated: "User Not Created Successfully !"
 }
