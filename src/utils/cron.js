1fea5ce9ee6d3f297813ca0ad2f95a521ad4bbef
Nithin M <nithin.muniraju@waycool.in>
2021-05-17 18:54:41 +0530

    commented cron for SO and PO

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -25,63 +25,63 @@ const guid = () => {
 
 // exporting the module 
 module.exports = () => {
-
-  // cron for autocheckout users 
-  schedule.scheduleJob('0 0 * * *', async () => {
-    let requestdId = guid();
-    let time = moment().format('DD-MM-YYYY HH:mm')
-    info('------------------------');
-    info('CRON TAB FOR AUTO CHECKOUT RUNNING STARTED', time);
-    cronLogger.info(`CRON-${requestdId} | ${time} | CRON TAB FOR AUTO CHECKOUT RUNNING`);
-    let isPricingSynced = await hitCronForAutoCheckoutAppUsers(requestdId);
-    if (isPricingSynced.success) {
-      cronLogger.info(`CRON-${requestdId} | ${time} | CRON TAB FOR AUTO CHECKOUT RUNNING SUCCESS !`);
-      info('CRON TAB FOR AUTO CHECKOUT SUCCESSFULLY FINISHED ', time);
-    }
-    else {
-      cronLogger.error(`CRON-${requestdId} | ${time} | CRON TAB FOR AUTO CHECKOUT RUNNING ERROR !`, JSON.stringify(isPricingSynced.error));
-      error('CRON TAB FOR AUTO CHECKOUT ERROR ', time, isPricingSynced.error);
-    }
-    info('------------------------');
-  });
-
-  
-  if(cluster.isMaster) {
-    // cron job for sync Purchase order
-    schedule.scheduleJob('0 */2 * * * *', async () => {
+  if (cluster.isMaster) {
+    // cron for autocheckout users 
+    schedule.scheduleJob('0 0 * * *', async () => {
       let requestdId = guid();
       let time = moment().format('DD-MM-YYYY HH:mm')
       info('------------------------');
-      info('CRON TAB RUNNING STARTED', time);
-      cronLogger.info(`CRON-${requestdId} | ${time} | STARTED FOR SYNC PURCHASE ORDER`);
-      let isPurchaseOrderUpdated = await hitCronToSyncPurchaseOrderData(requestdId);
-      if (isPurchaseOrderUpdated.success) {
-        cronLogger.info(`CRON-${requestdId} | ${time} | COMPLETED SUCCESSFULLY - `, JSON.stringify(isPurchaseOrderUpdated.data));
-        info('CRON TAB SUCCESSFULLY FINISHED ', time);
-      } else {
-        cronLogger.error(`CRON-${requestdId} | ${time} | ERROR - `, JSON.stringify(isPurchaseOrderUpdated.error));
-        error('CRON TAB ERROR ', time, isPurchaseOrderUpdated.error);
+      info('CRON TAB FOR AUTO CHECKOUT RUNNING STARTED', time);
+      cronLogger.info(`CRON-${requestdId} | ${time} | CRON TAB FOR AUTO CHECKOUT RUNNING`);
+      let isPricingSynced = await hitCronForAutoCheckoutAppUsers(requestdId);
+      if (isPricingSynced.success) {
+        cronLogger.info(`CRON-${requestdId} | ${time} | CRON TAB FOR AUTO CHECKOUT RUNNING SUCCESS !`);
+        info('CRON TAB FOR AUTO CHECKOUT SUCCESSFULLY FINISHED ', time);
+      }
+      else {
+        cronLogger.error(`CRON-${requestdId} | ${time} | CRON TAB FOR AUTO CHECKOUT RUNNING ERROR !`, JSON.stringify(isPricingSynced.error));
+        error('CRON TAB FOR AUTO CHECKOUT ERROR ', time, isPricingSynced.error);
       }
       info('------------------------');
     });
+
+
+
+    // cron job for sync Purchase order
+    // schedule.scheduleJob('0 */2 * * * *', async () => {
+    //   let requestdId = guid();
+    //   let time = moment().format('DD-MM-YYYY HH:mm')
+    //   info('------------------------');
+    //   info('CRON TAB RUNNING STARTED', time);
+    //   cronLogger.info(`CRON-${requestdId} | ${time} | STARTED FOR SYNC PURCHASE ORDER`);
+    //   let isPurchaseOrderUpdated = await hitCronToSyncPurchaseOrderData(requestdId);
+    //   if (isPurchaseOrderUpdated.success) {
+    //     cronLogger.info(`CRON-${requestdId} | ${time} | COMPLETED SUCCESSFULLY - `, JSON.stringify(isPurchaseOrderUpdated.data));
+    //     info('CRON TAB SUCCESSFULLY FINISHED ', time);
+    //   } else {
+    //     cronLogger.error(`CRON-${requestdId} | ${time} | ERROR - `, JSON.stringify(isPurchaseOrderUpdated.error));
+    //     error('CRON TAB ERROR ', time, isPurchaseOrderUpdated.error);
+    //   }
+    //   info('------------------------');
+    // });
 
     // cron job for sync Sales order
-    schedule.scheduleJob('0 */2 * * * *', async () => {
-      let requestdId = guid();
-      let time = moment().format('DD-MM-YYYY HH:mm')
-      info('------------------------');
-      info('CRON TAB RUNNING STARTED', time);
-      cronLogger.info(`CRON-${requestdId} | ${time} | STARTED FOR SYNC SALES ORDER`);
-      let isSalesOrderUpdated = await hitCronToSyncSalesOrderData(requestdId);
-    
-      if (isSalesOrderUpdated.success) {
-        cronLogger.info(`CRON-${requestdId} | ${time} | COMPLETED SUCCESSFULLY - `, JSON.stringify(isSalesOrderUpdated.data));
-        info('CRON TAB SUCCESSFULLY FINISHED ', time);
-      } else {
-        cronLogger.error(`CRON-${requestdId} | ${time} | ERROR - `, JSON.stringify(isSalesOrderUpdated.error));
-        error('CRON TAB ERROR ', time, isSalesOrderUpdated.error);
-      }
-      info('------------------------');
-    });
+    // schedule.scheduleJob('0 */2 * * * *', async () => {
+    //   let requestdId = guid();
+    //   let time = moment().format('DD-MM-YYYY HH:mm')
+    //   info('------------------------');
+    //   info('CRON TAB RUNNING STARTED', time);
+    //   cronLogger.info(`CRON-${requestdId} | ${time} | STARTED FOR SYNC SALES ORDER`);
+    //   let isSalesOrderUpdated = await hitCronToSyncSalesOrderData(requestdId);
+
+    //   if (isSalesOrderUpdated.success) {
+    //     cronLogger.info(`CRON-${requestdId} | ${time} | COMPLETED SUCCESSFULLY - `, JSON.stringify(isSalesOrderUpdated.data));
+    //     info('CRON TAB SUCCESSFULLY FINISHED ', time);
+    //   } else {
+    //     cronLogger.error(`CRON-${requestdId} | ${time} | ERROR - `, JSON.stringify(isSalesOrderUpdated.error));
+    //     error('CRON TAB ERROR ', time, isSalesOrderUpdated.error);
+    //   }
+    //   info('------------------------');
+    // });
   }
 }
